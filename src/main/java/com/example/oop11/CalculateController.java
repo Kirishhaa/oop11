c3faeca0e90a1641c3d3e8e923c15545f34a4dd8
Kirishhaa <kiryusha.chugunov@gmail.com>
2023-04-04 16:29:42 +0300

    getMap

--- src/main/java/com/example/oop11/CalculateController.java
+++ src/main/java/com/example/oop11/CalculateController.java
@@ -2,13 +2,13 @@ package com.example.oop11;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculateController {
 
-    @RequestMapping("/calc")
+    @GetMapping("/calc")
     public String calculate(
             @RequestParam double start,
             @RequestParam double end,
